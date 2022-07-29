6f63368a5788a26f61eb8a0fb1330797f5fe6dfa
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-07-29 21:34:26 +0300

    часть 1 закончил на 1 час 50 мин Loader.js useFetching.js PostService.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 
@@ -9,6 +9,9 @@ import PostFIlter from "./components/PostFilter/PostFIlter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 
 function App() {
@@ -22,7 +25,11 @@ function App() {
 
     const [filter, setFilter] = useState({query: '', sort: ''});
     const [visible, setVisible] = useState(false);
-    const sortedAndSearched = usePosts(posts, filter.sort, filter.query)
+    const sortedAndSearched = usePosts(posts, filter.sort, filter.query);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const post = await PostService.getAll();
+        setPosts(post.data);
+    } )
 
     const createPost = (newPost) => {
         if(newPost.body !== '' && newPost.title !== '') {
@@ -31,12 +38,27 @@ function App() {
         }
     }
 
+    // async function fetchPosts() {
+    //     setIsPostsLoading(true);
+    //     setTimeout(async () => { //таймаут для наглядности
+    //         const response = await PostService.getAll();
+    //         setPosts(response.data);
+    //         setIsPostsLoading(false);
+    //     }, 1000);
+    // }
+
     const deletePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
     }
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
 
     return (
         <div className="App">
+
+            <MyButton onClick={fetchPosts}>GetPosts</MyButton>
             <MyButton onClick={() => setVisible(true)}>
                 Создать пост!
             </MyButton>
@@ -46,10 +68,13 @@ function App() {
 
             <hr style={{margin: '15px'}}/>
             <PostFIlter filter={filter} setFilter={setFilter}/>
-            <PostList
-                deletePost={deletePost}
-                posts={sortedAndSearched}
-                title={'Список постов про разные языки'}/>
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', margin: 50}}><Loader/></div>
+                : <PostList
+                    deletePost={deletePost}
+                    posts={sortedAndSearched}
+                    title={'Список постов про разные языки'}/>}
+
             <Counter/>
             <ClassCounter/>
         </div>
