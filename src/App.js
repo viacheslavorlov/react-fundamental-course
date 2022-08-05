ccb916d84639bfbe4d793d702bb8e79b984b9c43
viacheslavorlov <viacheslavorlov@gmail.com>
2022-08-05 16:14:38 +0300

    react fundamentals 7 часть 2 часa 14 мин react-router-dom installed

--- src/App.js
+++ src/App.js
@@ -1,92 +1,14 @@
 import './styles/App.css';
-import React, {useEffect, useState} from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
-
-import PostList from "./components/PostList/PostList";
-import PostForm from "./components/PostForm/PostForm";
-import PostFIlter from "./components/PostFilter/PostFIlter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import {usePosts} from "./hooks/usePosts";
-import PostService from "./API/PostService/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import {useFetching} from "./hooks/useFetching";
-import {getPageCount} from "./utils/pages";
-import Pagination from "./components/Pagination/Pagination";
+import React from "react";
+import {BrowserRouter} from 'react-router-dom'
+import Posts from "./components/Pages/Posts";
 
 function App() {
-    const [posts, setPosts] = useState([]);
-    const [filter, setFilter] = useState({query: '', sort: ''});
-    const [visible, setVisible] = useState(false);
-    const [totalPages, setTotalPages] = useState(0);
-    const [limit, setLimit] = useState(10);
-    const [page, setPage] = useState(1);
-    const sortedAndSearched = usePosts(posts, filter.sort, filter.query);
-
-
-
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const post = await PostService.getAll(limit, page);
-        setPosts(post.data);
-        const totalCount = post.headers['x-total-count'];
-        console.log(totalCount);
-        setTotalPages(getPageCount(totalCount, limit));
-    });
-
-    const createPost = (newPost) => {
-        if(newPost.body !== '' && newPost.title !== '') {
-            setPosts([...posts, newPost]);
-            setVisible(false);
-        }
-    }
-
-    // async function fetchPosts() {
-    //     setIsPostsLoading(true);
-    //     setTimeout(async () => { //таймаут для наглядности
-    //         const response = await PostService.getAll();
-    //         setPosts(response.data);
-    //         setIsPostsLoading(false);
-    //     }, 1000);
-    // }
-
-    const deletePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id));
-    }
-    useEffect(() => {
-        fetchPosts();
-    }, [page]);
-
-    const changePage = (page) => {
-        setPage(page);
-    }
-
     return (
-        <div className="App">
-
-            <MyButton onClick={fetchPosts}>GetPosts</MyButton>
-            <MyButton onClick={() => setVisible(true)}>
-                Создать пост!
-            </MyButton>
-            <MyModal visible={visible} setVisible={setVisible}>
-                <PostForm createPost={createPost} setVisible={setVisible}/>
-            </MyModal>
-
-            <hr style={{margin: '15px'}}/>
-            <PostFIlter filter={filter} setFilter={setFilter}/>
-
-            {isPostsLoading
-                ? <div style={{display: 'flex', justifyContent: 'center', margin: 50}}><Loader/></div>
-                : postError ? <h1>произошла ошибка: {postError}</h1> : <PostList
-                    deletePost={deletePost}
-                    posts={sortedAndSearched}
-                    title={'Список постов про разные языки'}/>}
-            <Pagination page={page}
-                        changePage={changePage}
-                        totalPages={totalPages}/>
-
-            <Counter/>
-            <ClassCounter/>
+        <div>
+            <BrowserRouter>
+                <Posts/>
+            </BrowserRouter>
         </div>
     )
 }
